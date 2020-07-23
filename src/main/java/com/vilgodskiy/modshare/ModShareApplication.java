19ea001389fb30c735d93a18bc2050c5c0ce526a
vilgodskiy <vilgodskiy@element-soft.com>
2020-07-23 20:08:21 +0300

    Initial commit

---
+++ src/main/java/com/vilgodskiy/modshare/ModShareApplication.java
@@ -0,0 +1,13 @@
+package com.vilgodskiy.modshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilgodskiy/modshare/ModShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilgodskiy.modshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
