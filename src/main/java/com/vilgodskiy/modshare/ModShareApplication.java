ec753b7764f144c91a2a0956d23ff348764f7d9c
vilgodskiy <vilgodskiy@element-soft.com>
2020-07-24 10:14:44 +0300

    Configured build.gradle

--- src/main/java/com/vilgodskiy/modshare/ModShareApplication.java
+++ src/main/java/com/vilgodskiy/modshare/ModShareApplication.java
@@ -3,6 +3,9 @@ package com.vilgodskiy.modshare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Vilgodskiy Sergey 24.07.2020
+ */
 @SpringBootApplication
 public class ModShareApplication {
 
